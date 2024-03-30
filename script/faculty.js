4f7423bfa70e42ba5d01cb3f9c07cee00244ac7e
Ehsan akefi <47789824+ehsanakefi@users.noreply.github.com>
2024-03-30 18:50:47 +0330

    Update faculty.js

--- script/faculty.js
+++ script/faculty.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
           val.image == ""
             ? `<div class=${index % 2 == 0 ? "empty_image_box1" : "empty_image_box2"
             }></div>`
-            : ` <img src=${val.image} class="profile_img_team" />`;
+            : ` <img alt="image profile user" src=${val.image} class="profile_img_team" />`;
 
         facultyResult += `<div class="card">
               <div class="image_box">
@@ -21,22 +21,22 @@ document.addEventListener("DOMContentLoaded", function () {
                 <p class="status_text_team">${val.field}</p>
                 <div class="icons_team">
                   ${val.linkedin
-            ? `<a aria-label="linkedin" href="${val.linkedin}" class="tooltip" target="_blank"><img src="./image/linkedin.png" class="icons_team_img" />
+            ? `<a aria-label="linkedin" href="${val.linkedin}" class="tooltip" target="_blank"><img alt="icon small LinkedIn" src="./image/linkedin.png" class="icons_team_img" />
                       <span class="tooltiptext">${val.name} 's LinkedIn</span></a>`
             : ""
           }
                   ${val.github
-            ? `<a aria-label="github" href="${val.github}" class="tooltip" target="_blank"><img src="./image/git.png" class="icons_team_img" />
+            ? `<a aria-label="github" href="${val.github}" class="tooltip" target="_blank"><img alt="icon small GitHub" src="./image/git.png" class="icons_team_img" />
                       <span class="tooltiptext">${val.name} 's GitHub</span></a>`
             : ""
           }
                   ${val.google_scholar
-            ? `<a aria-label="google-scholar" href="${val.google_scholar}" class="tooltip" target="_blank"><img src="./image/google.png" class="icons_team_img" />
+            ? `<a aria-label="google-scholar" href="${val.google_scholar}" class="tooltip" target="_blank"><img alt="icon small Google Scholar" src="./image/google.png" class="icons_team_img" />
                       <span class="tooltiptext">${val.name} 's Google Scholar</span></a>`
             : ""
           }
                   ${val.homepage
-            ? `<a aria-label="homepage" href="${val.homepage}" class="tooltip" target="_blank"><img src="./image/website.png" class="icons_team_img" />
+            ? `<a aria-label="homepage" href="${val.homepage}" class="tooltip" target="_blank"><img alt="icon small Website" src="./image/website.png" class="icons_team_img" />
                       <span class="tooltiptext">${val.name} 's Website</span></a>`
             : ""
           }
